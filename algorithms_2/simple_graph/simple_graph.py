66e1cb37d36d93bd2a1a75effda4b5c204f7310b
Oleg Marintsov <awwert@yandex.ru>
2021-04-08 16:16:56 +0300

    Add DepthFirstSearch method to SimpleGraph

--- algorithms_2/simple_graph/simple_graph.py
+++ algorithms_2/simple_graph/simple_graph.py
@@ -2,6 +2,7 @@ class Vertex:
 
     def __init__(self, val):
         self.Value = val
+        self.Hit = False
   
 class SimpleGraph:
 
@@ -28,3 +29,45 @@ class SimpleGraph:
 
     def RemoveEdge(self, v1, v2):
         self.m_adjacency[v1][v2] = self.m_adjacency[v2][v1] = 0
+
+    def DepthFirstSearch(self, VFrom, VTo):
+        self.clear_vertices()
+        path = []
+        current = VFrom
+
+        while True:
+            self.visit(current)
+            path.append(current)
+
+            if self.IsEdge(current, VTo):
+                self.visit(VTo)
+                path.append(VTo)
+                return [self.vertex[v] for v in path]
+
+            unvisited = self.get_unvisited_vertices(current)
+            if unvisited:
+                current = unvisited[0]
+                continue
+
+            path.pop()
+            if path:
+                current = path.pop()
+            else:
+                return []
+
+    def clear_vertices(self):
+        for vertex in self.vertex:
+            if vertex is not None:
+                vertex.Hit = False
+
+    def visit(self, v):
+        self.vertex[v].Hit = True
+
+    def get_edges(self, v):
+        return {v2 for v2 in range(self.max_vertex) if self.IsEdge(v, v2)}
+
+    def is_visited(self, v):
+        return self.vertex[v].Hit
+
+    def get_unvisited_vertices(self, v):
+        return [v for v in self.get_edges(v) if not self.is_visited(v)]
